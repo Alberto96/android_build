4ef942fac298365b49f762019a0cf92740c6b719
Alberto96 <albertoguanti@gmail.com>
2013-09-04 16:15:25 +0200

    Revert "test 5 ....."
    This reverts commit e60f7eca178707b4beb3e518800f42ccf5924ebc.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -79,7 +79,7 @@ TARGET_arm_CFLAGS :=    -O3 \
                         -funswitch-loops \
                         -funsafe-loop-optimizations \
                         -ftree-vectorize \
-                        -pipe $(STRICT_ALIASING_WARNINGS) -Wno-error=strict-aliasing --enable-compile-warnings=yes
+                        -pipe $(STRICT_ALIASING_WARNINGS) -Wno-error=strict-aliasing
 
 # THUMB SUX BALLS. but we'll still compile it here and get rid of always true shit.
 TARGET_thumb_CFLAGS :=  -mthumb \
@@ -87,7 +87,7 @@ TARGET_thumb_CFLAGS :=  -mthumb \
                         -fomit-frame-pointer \
                         -fstrict-aliasing \
                         -funsafe-math-optimizations \
-                        -pipe $(STRICT_ALIASING_WARNINGS) -Wno-error=strict-aliasing --enable-compile-warnings=yes
+                        -pipe $(STRICT_ALIASING_WARNINGS) -Wno-error=strict-aliasing
 
 #SHUT THE F$#@ UP!
 TARGET_arm_CFLAGS +=    -Wno-unused-parameter \
@@ -138,7 +138,7 @@ TARGET_GLOBAL_CFLAGS += \
 			-D_FORTIFY_SOURCE=0 \
 			-fno-short-enums \
 			-pipe \
-			$(arch_variant_cflags) $(STRICT_ALIASING_WARNINGS) -Wno-error=strict-aliasing --enable-compile-warnings=yes
+			$(arch_variant_cflags) $(STRICT_ALIASING_WARNINGS) -Wno-error=strict-aliasing
 
 android_config_h := $(call select-android-config-h,linux-arm)
 TARGET_ANDROID_CONFIG_CFLAGS := -include $(android_config_h) -I $(dir $(android_config_h))
